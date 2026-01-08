0e667ca16c64ca2bcdc2d7ff26cbeb85e5aa40ba
zozo <zozo@zozo.com>
2026-01-08 16:10:01 +0300

    refactored the profile controller

---
+++ app/Services/ProfileService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Str;
+
+class ProfileService{
+  protected $request;
+  protected $validInfo;
+
+  public function setRequest(ProfileUpdateRequest $request){
+    $this->request = $request;
+    $this->validInfo = $request->validated();
+  }
+  public function update(){
+    $request = $this->request;
+    $img = $request->file('file') ?? null;
+    $cover_img = $request->file('cover') ?? null;
+    if($img){
+      $info['image'] = $img->store('avatars', 'public');
+      unset($info['file']);
+    }
+    if($cover_img){
+      $info['cimage'] = $cover_img->store('avatars', 'public');
+      unset($info['cover']);
+    }
+    $request->user()->fill($info);
+    if ($request->user()->isDirty('email')) {
+      $request->user()->email_verified_at = null;
+    }
+    $request->user()->save();
+  }
+}
+
+      // if($request->file('image')){
+      //   $productProps['image_path'] = $request->file('image')->store('products_images', 'public');
+      //   unset($productProps['image']);
+      // }
